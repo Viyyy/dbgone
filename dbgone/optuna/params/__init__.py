db65c8a80edcf380387060349e2b827118272ec4
Viyyy <1793707472@qq.com>
2025-09-04 15:46:06 +0800

    feat(optuna/params): 添加调优参数类和基类
    - 添加了 `OptParamBase` 类作为所有调优参数类的基类，定义了调优参数的基本属性和抽象方法 `add_to_trial` 与 `to_echart_axis`。
    - 实现了 `OptInt`、`OptChoice`、`OptUniform`、`OptLogUniform`、`OptDiscreteUniform` 和 `OptFloat` 类，这些类继承自 `OptParamBase`，并实现了具体的 `add_to_trial` 和 `to_echart_axis` 方法。
    - 添加了 `OptType` 枚举类，用于表示不同的调优参数类型，并实现了 `__call__` 方法以根据枚举值生成相应的调优参数实例。

---
+++ dbgone/optuna/params/__init__.py
@@ -0,0 +1,19 @@
+from .main import (
+    OptInt,
+    OptChoice,
+    OptUniform,
+    OptLogUniform,
+    OptDiscreteUniform,
+    OptFloat,
+    OptType,
+)
+
+__all__ = [
+    "OptInt",
+    "OptChoice",
+    "OptUniform",
+    "OptLogUniform",
+    "OptDiscreteUniform",
+    "OptFloat",
+    "OptType",
+]
